65f535b92d1452bb1899958b7204509945341d3c
Anish <138765564+anish170805@users.noreply.github.com>
2025-08-11 07:57:56 +0000

    db url changed

--- config/db.js
+++ config/db.js
@@ -2,15 +2,7 @@ import mysql from 'mysql2';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+const pool = mysql.createPool(`mysql://root:LmDFjpLfLbWgiHIkzDJgHTnYLyfUbPod@mysql.railway.internal:3306/railway`);
 
 (async () => {
   let connection;
